3bdebc44dbbeeba56adc8a9982ec7f1646b6bb0c
Jakub Gołaszewski <jgolaszewski6@gmail.com>
2023-06-03 09:53:36 +0200

    move firebase functionality to repository

---
+++ lib/repository/items_repository.dart
@@ -0,0 +1,39 @@
+import 'package:cantwait28/models/item_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ItemsRepository {
+  Stream<List<ItemModel>> getItemsStream() {
+    return FirebaseFirestore.instance
+        .collection('items')
+        .orderBy('release_date')
+        .snapshots()
+        .map((querySnapshot) {
+      return querySnapshot.docs.map((doc) {
+        return ItemModel(
+          id: doc.id,
+          title: doc['title'],
+          imageURL: doc['image_url'],
+          releaseDate: (doc['release_date'] as Timestamp).toDate(),
+        );
+      }).toList();
+    });
+  }
+
+  Future<void> remove({required String id}) {
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
+
+  Future<void> add({
+    required String title,
+    required String imageURL,
+    required DateTime releaseDate,
+  }) async {
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'image_url': imageURL,
+        'release_date': releaseDate,
+      },
+    );
+  }
+}
